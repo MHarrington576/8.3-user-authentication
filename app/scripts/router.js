8e429745b82afff511498f0900235d20afd22451
Matthew Harrington <mattharrington576@gmail.com>
2016-11-25 17:11:22 -0500

    Restarting project

---
+++ app/scripts/router.js
@@ -0,0 +1,37 @@
+var Backbone = require('backbone');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var setUpParse = require('./parseUtilities').setUpParse;
+var LoginContainer = require('./components/login.jsx').LoginContainer;
+
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    '': 'login',
+    'messages/': 'messageboard'
+  },
+
+  initialize: function(){
+    setUpParse('tiyfeefall2016', 'parietinaeumbra');
+  },
+
+  login: function(){
+    ReactDOM.render(
+      React.createElement(LoginContainer),
+      document.getElementById('app')
+    );
+  },
+
+  messageboard: function(){
+    ReactDOM.render(
+      React.createElement(MessageboardContainer),
+      document.getElementById('app')
+    );
+  }
+});
+
+var router = new AppRouter();
+
+module.exports = {
+  router: router
+};
